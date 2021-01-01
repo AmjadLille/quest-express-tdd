73f294c52495e3d30a52091a4c0585a5eb19a872
AmjadLille <sammanamjad86@gmail.com>
2021-01-01 21:11:43 +0100

    fin Quest

--- connection.js
+++ connection.js
@@ -1,20 +1,21 @@
 // connection.js
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
+  port: "3306",
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
-    console.error('error connecting: ' + err.stack);
+    console.error("error connecting: " + err.stack);
     return;
   }
 
-  console.log('connected as id ' + connection.threadId);
+  console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
